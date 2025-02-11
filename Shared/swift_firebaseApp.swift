734a8c73dd755fd046409c39bfd9b4313e84a404
vignesh <johnvick281997@gmail.com>
2025-02-11 07:58:49 +0530

    Initial Commit

---
+++ Shared/swift_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_firebaseApp.swift
+//  Shared
+//
+//  Created by vignesh on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
